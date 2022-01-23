a6d2bedc38d9dab8657b45448834e93cd5bd0049
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-01-23 11:18:35 +0330

    Ch05-L07-permissions

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Product
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
 
 
 class MyCreatorTokenSerializer(TokenObtainPairSerializer):
@@ -61,7 +63,16 @@ def getProduct(request, pk):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
