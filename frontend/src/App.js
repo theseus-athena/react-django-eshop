b5eace7377ef29766d6bc7bb63d33a8c2b8edd73
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-02-19 09:58:54 +0330

    Ch06-L02-Create-Login-Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 
 
@@ -15,6 +16,7 @@ function App() {
       <main>
         <Container className='py-3'>
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
         </Container>
