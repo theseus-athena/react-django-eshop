6c6bc57b7473f527c938a979450a093428a9251a
Theseus-Athena <theseus.me@outlook.com>
2022-02-26 21:35:47 +0330

    Ch06-L04-Register-Redux

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,7 +2,12 @@ import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
+
     USER_LOGOUT,
+
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
 } from '../constants/userConstants'
 
 import axios from 'axios'
@@ -52,4 +57,51 @@ export const userLogoutAction = () => (dispatch) => {
     dispatch({
         type: USER_LOGOUT
     })
+}
+
+export const userRegisterAction = (name, email, password) => async (dispatch) => {
+    try {
+        dispatch({ // USER_REGISTER_REQUEST
+            type: USER_REGISTER_REQUEST,
+        })
+
+        const config = {// request setting
+            header: {
+                'Content-type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.post(// register request
+            '/api/v1/users/register/',
+            { 'name': name, 'email': email, 'password': password },
+            config
+        )
+
+        dispatch({// USER_REGISTER_SUCCESS
+            type: USER_REGISTER_SUCCESS,
+            payload: data
+        })
+
+        dispatch({// USER_LOGIN_REQUEST
+            type: USER_LOGIN_REQUEST,
+        })
+
+        dispatch({// USER_LOGIN_SUCCESS
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem(
+            'userInfo',
+            JSON.stringify(data)
+        )
+
+    } catch (error) {
+        dispatch({// USER_REGISTER_FAIL
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
 }
