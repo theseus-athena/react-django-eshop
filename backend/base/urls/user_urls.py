17edc8e301b0ce6bac3382890153e216550f754f
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-02-27 22:37:02 +0330

    Ch06-L06-update-user-view

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -4,6 +4,7 @@ from base.views import user_views as views
 urlpatterns = [
     path('login/', views.MyCreatorTokenView.as_view(), name='token-obtain-pair'),
     path('profile/', views.getUserProfile, name='users-profile'),
+    path('profile/update/', views.updateUserProfile, name='user-profile-update'),
     path('', views.getUsers, name='users'),
     path('register/', views.registerUser, name='register')
 ]
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -34,6 +34,24 @@ def getUserProfile(request):
     return Response(serializer.data)
 
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    data = request.data
+
+    user.first_name = data['name']
+    user.email = data['email']
+    user.username = data['email']
+
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+    serializer = UserSerializerWithToken(user, many=False)
+    return Response(serializer.data)
+
+
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
 def getUsers(request):
