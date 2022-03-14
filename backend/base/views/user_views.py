505353572b9f7db14ecc5af16e4b2911338b49ac
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-03-14 13:14:30 +0330

    Ch06-L12-Modify-Register-View

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,3 +1,4 @@
+from tracemalloc import is_tracing
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
@@ -7,6 +8,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
+
+
+def emailSender(data):
+    pass
 
 
 class MyCreatorTokenSerializer(TokenObtainPairSerializer):
@@ -63,17 +68,53 @@ def getUsers(request):
 @api_view(['POST'])
 def registerUser(request):
     data = request.data
+    username = data['email']
 
     try:
         user = User.objects.create(
             first_name=data['name'],
             username=data['email'],
             email=data['email'],
-            password=make_password(data['password'])
+            password=make_password(data['password']),
+            is_active=False,
         )
 
-        serializer = UserSerializerWithToken(user, many=False)
-        return Response(serializer.data)
+        # TODO: send activation email to user
+        data = {}
+        result = emailSender(data)
+
+        if result:
+            message = {
+                'detail': 'Welcome to E-Shop. You should verify your email.Please check your Inbox. We sent an email.'
+            }
+            return Response(message, status=status.HTTP_201_CREATED)
+        else:
+            message = {
+                'detail': 'There\'s a problem on sending email.'
+            }
+            return Response(message, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     except:
-        message = {'detail': 'User with this email already exists.'}
-        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+        user = User.objects.get(username=username)
+
+        if user and not user.is_active:
+
+            # TODO: send activation email to user
+            data = {}
+            result = emailSender(data)
+
+            if result:
+                message = {
+                    'detail': 'User with this email already exists but is not active. You should verify your email.Please check your Inbox. We sent an email.'
+                }
+                return Response(message, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                message = {
+                    'detail': 'There\'s a problem on sending email.'
+                }
+                return Response(message, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        elif user and user.is_active:
+            message = {
+                'detail': 'User with this email already exists and is active too. Please login.'
+            }
+            return Response(message, status=status.HTTP_400_BAD_REQUEST)
