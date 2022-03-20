41a5bf7abfd4f27aa451ab7852e931e62516600e
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2022-03-20 20:03:05 +0330

    Ch06-L14-Activation-User

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('profile/', views.getUserProfile, name='users-profile'),
     path('profile/update/', views.updateUserProfile, name='user-profile-update'),
     path('', views.getUsers, name='users'),
-    path('register/', views.registerUser, name='register')
+    path('register/', views.registerUser, name='register'),
+    path('verify/<str:token>/', views.verifyEmail, name='verify-email'),
 ]
