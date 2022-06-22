f6990d8cf54edd10a49cd6e85d4d6e6786210283
Theseus-Athena <theseus.me@outlook.com>
2022-06-22 20:20:13 +0430

    Ch07-L05-Order-View-And-Url

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,8 +1,10 @@
 from django.db.models import fields
-from rest_framework import serializers
-from .models import Product
 from django.contrib.auth.models import User
+
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import serializers
+
+from base.models import Product,Order,OrderItem,ShippingAddress
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -44,3 +46,43 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+        
+        
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    orderItems = serializers.SerializerMethodField(read_only=True)
+    shippingAddress = serializers.SerializerMethodField(read_only=True)
+    user = serializers.SerializerMethodField(read_only=True)
+    
+    
+    class Meta:
+        model = Order
+        fields = '__all__'
+        
+    
+    
+    def get_orderItems(self,obj):
+        items = obj.orderitem_set.all()
+        return OrderItemSerializer(items,many=True).data
+
+    def get_shippingAddress(self,obj):
+        try:
+            address = ShippingAddressSerializer(obj.shippingaddress).data
+        except:
+            address = False
+        return address
+    
+    def get_user(self,obj):
+        return UserSerializer(obj.user,many=False).data
+
