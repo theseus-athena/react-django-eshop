838df0b6332960580b1dfbf29785d49aabae7f9f
Theseus-Athena <theseus.me@outlook.com>
2022-06-28 16:32:13 +0430

    Ch07-L07-Get-Order-EndPoint

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -3,5 +3,6 @@ from base.views import order_views as views
 
 
 urlpatterns = [
-    path('add/',views.addOrderItems,name='orders-name'),
+    path('add/',views.addOrderItems,name='orders-add'),
+    path('<str:pk>/',views.getOrderById,name='user-order')
 ]
--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -57,4 +57,22 @@ def addOrderItems(request):
             product.save()
         serializer = OrderSerializer(order,many=False)
         return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrderById(request,pk):
+    try:
+        user = request.user
+        order = Order.objects.get(_id=pk)
         
+        if user.is_staff or order.user == user :
+            serializer = OrderSerializer(order,many=False)
+            return Response(serializer.data)
+        else:
+            return Response({'detail':'Not authorized to view this order !'},
+                            status=status.HTTP_400_BAD_REQUEST)  
+    except:
+        return Response({'detail':'Order does not exist !'},
+                        status=status.HTTP_404_NOT_FOUND)     
