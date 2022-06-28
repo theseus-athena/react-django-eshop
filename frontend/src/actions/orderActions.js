fe83645c130d8523ae9befdd32c1ba8c73f50cd2
Theseus-Athena <theseus.me@outlook.com>
2022-06-28 17:43:01 +0430

    Ch07-L08-Order-Details-Redux

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -2,6 +2,10 @@ import {
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
     ORDER_CREATE_FAIL,
+
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_FAIL,
 } from '../constants/orderConstants'
 
 import { CART_CLEAR_ITEMS } from '../constants/cartConstatns'
@@ -30,6 +34,31 @@ export const createOrder = (order) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: ORDER_CREATE_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+}
+
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: ORDER_DETAILS_REQUEST })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const authRequestAxios = axiosInstance(userInfo, dispatch)
+
+        const { data } = await authRequestAxios.get(
+            `/api/v1/orders/${id}/`
+        )
+
+        dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data })
+
+    } catch (error) {
+        dispatch({
+            type: ORDER_DETAILS_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message
