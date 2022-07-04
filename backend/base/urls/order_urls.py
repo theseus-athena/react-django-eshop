08145b2a7f4b52990c5f07417f37c634731d167a
Theseus-Athena <theseus.me@outlook.com>
2022-07-04 21:57:03 +0430

    Ch07-L10-Orders-In-Profile-Page

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -4,5 +4,6 @@ from base.views import order_views as views
 
 urlpatterns = [
     path('add/',views.addOrderItems,name='orders-add'),
+    path('my/',views.getMyOrders,name='user-orders'),
     path('<str:pk>/',views.getOrderById,name='user-order')
 ]
