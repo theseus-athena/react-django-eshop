f2606597acd1619ed52d29f3ce5fae992dc5daeb
Theseus-Athena <theseus.me@outlook.com>
2022-09-03 22:15:44 +0430

    Ch08-L07-Create-Transaction-in-Idpay

---
+++ backend/base/utils/pay_utils.py
@@ -0,0 +1,41 @@
+import requests, json
+from X_API_KEY import X_API_KEY
+from base.models import Idpay
+
+IDPAY_HEADER = {
+    'Content-Type':'application/json',
+    'X-API-KEY':X_API_KEY,
+    'X-SANDBOX':'1'
+}
+
+
+def idpayCreatePay(user, order):
+    body = {
+        'order_id' : str(order._id),
+        'amount': (order.totalPrice * 10),
+        'mail' : user.email,
+        'callback' : 'http://127.0.0.1:8000/api/v1/air/result/'
+    }
+    
+    response = requests.post(
+        'https://api.idpay.ir/v1.1/payment',
+        data=json.dumps(body),
+        headers = IDPAY_HEADER,
+    )
+    
+    return response
+
+
+def idpayCreateDB(user, order, id):
+    try:
+        idpay = Idpay.objects.create(
+            user = user,
+            order = order,
+            transId = id,
+            lastStatus = 0 ,
+            amountCreate = (order.totalPrice * 10)
+        )
+        return True
+    except:
+        return False
+        
--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 
 from base.models import Product,Order,OrderItem,ShippingAddress
 from base.serializers import OrderSerializer
+from base.utils.pay_utils import idpayCreateDB, idpayCreatePay
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
